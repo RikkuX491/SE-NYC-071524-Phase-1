b6b83876290d699e0e54ae3ab643d540fcbe6737
Ricardo Guerra <RikkuG491@gmail.com>
2024-07-24 16:09:56 -0400

    Added example code from the lecture

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ fetch('http://localhost:3000/foods')
     foods.forEach(food => {
         addFoodImageToRestaurantMenu(food)
     })
+    // console.log(foods)
 })
 
 function addFoodImageToRestaurantMenu(food){
@@ -42,9 +43,51 @@ newFoodForm.addEventListener('submit', (event) => {
         description: newDescriptionInputElement.value
     }
 
-    addFoodImageToRestaurantMenu(newFood)
+    // Optimistic Rendering approach - Render new food image first, then make POST request
+    // addFoodImageToRestaurantMenu(newFood)
 
-    // write your code here
+    // fetch('http://localhost:3000/foods', {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(newFood)
+    // })
+
+    // Pessimistic rendering approach - Make POST request first, then render food image in response to POST request
+    // fetch('http://localhost:3000/foods', {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(34)
+    // })
+    // .then(response => response.json())
+    // .then(newFood => addFoodImageToRestaurantMenu(newFood))
+    // .catch(() => alert("Error: Unable to add new food!"))
+    
+    // We can use .catch() to do something with the error
+    // .catch(error => console.log(error))
+
+    fetch('http://localhost:3000/foods', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newFood)
+    })
+    .then(response => {
+        if(response.ok){
+            response.json().then(newFood => addFoodImageToRestaurantMenu(newFood))
+        }
+        // else if(response.status === 400){
+        //     alert("Error: A bad request was made!")
+        // }
+        else{
+            // alert("Error: Unable to add new food!")
+            alert(`${response.status}: ${response.statusText}`)
+        }
+    })
 
     newFoodForm.reset()
 })
