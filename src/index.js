4e058fbef5bdc6ccd9d8a2160e1508fb90cf2c48
Ricardo Guerra <RikkuG491@gmail.com>
2024-07-26 11:30:25 -0400

    Added example code from the lecture

--- src/index.js
+++ src/index.js
@@ -38,3 +38,71 @@ function displayFoodDetails(food){
     const foodDescriptionDisplayElement = document.getElementById('description-display')
     foodDescriptionDisplayElement.textContent = food.description
 }
+
+// Example code
+// Access <ul> element where the cryptocurrency data will be displayed inside of (in a <li> element for each cryptocurrency)
+const cryptocurrencyListElement = document.getElementById('cryptocurrency-list')
+
+// Access the <select> element which will be used to filter the cryptocurrencies that are displayed on the web page
+const cryptocurrencyFilterElement = document.getElementById('cryptocurrency-filter')
+
+fetch("https://api.coincap.io/v2/assets")
+.then(response => {
+    if(response.ok){
+        // alert("GET request was successful!")
+        response.json().then(apiDataObject => {
+            // Iterate through our data and display all cryptocurrency data on the web page
+            // apiDataObject.data.forEach(cryptocurrency => {
+            //     const liElement = document.createElement('li')
+            //     liElement.textContent = `${cryptocurrency.name} (${cryptocurrency.symbol}): Rank # ${cryptocurrency.rank}`
+            //     cryptocurrencyListElement.appendChild(liElement)
+            // })
+
+            // Get the top ten cryptocurrencies and store them into a variable
+            // const topTenCryptocurrencies = apiDataObject.data.filter(cryptocurrency => {
+            //     return Number(cryptocurrency.rank) <= 10
+            // })
+            
+            // Iterate through our filtered data and display the top ten cryptocurrencies (by rank)
+            // topTenCryptocurrencies.forEach(addCryptocurrencyToList)
+
+            displayCryptocurrencyData(apiDataObject.data)
+
+            // Add an event listener so that the <select> element will listen for a change event and execute code in response to the change
+            cryptocurrencyFilterElement.addEventListener('change', () => {
+                displayCryptocurrencyData(apiDataObject.data)
+            })
+        })
+    }
+    else{
+        alert("Error: Unable to retrieve cryptocurrency data!")
+    }
+})
+
+function addCryptocurrencyToList(cryptocurrency) {
+    const liElement = document.createElement('li')
+    liElement.textContent = `${cryptocurrency.name} (${cryptocurrency.symbol}): Rank # ${cryptocurrency.rank}`
+    cryptocurrencyListElement.appendChild(liElement)
+}
+
+function displayCryptocurrencyData(cryptocurrencies){
+    cryptocurrencyListElement.innerHTML = ""
+
+    if(cryptocurrencyFilterElement.value === "all"){
+        cryptocurrencies.forEach(addCryptocurrencyToList)
+    }
+    else if(cryptocurrencyFilterElement.value === "less_than"){
+        cryptocurrencies.forEach(cryptocurrency => {
+            if(Number(cryptocurrency.rank) <= 50){
+                addCryptocurrencyToList(cryptocurrency)
+            }
+        })
+    }
+    else if(cryptocurrencyFilterElement.value === "greater_than"){
+        cryptocurrencies.forEach(cryptocurrency => {
+            if(Number(cryptocurrency.rank) > 50){
+                addCryptocurrencyToList(cryptocurrency)
+            }
+        })
+    }
+}
